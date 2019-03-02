3761d2f9d98c098d85be76652f0b54e97b1fae54
frameyl <frameyl@163.com>
2019-03-03 00:16:31 +0800

    Move colums to global const

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -4,6 +4,45 @@ import _ from 'lodash'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
+const columns=[
+  {
+    Header: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        accessor: "firstName"
+      },
+      {
+        Header: "Last Name",
+        id: "lastName",
+        accessor: d => d.lastName
+      }
+    ]
+  },
+  {
+    Header: "Info",
+    columns: [
+      {
+        Header: "Age",
+        accessor: "age",
+        aggregate: vals => _.round(_.mean(vals)),
+        Aggregated: row => {
+          return (
+            <span>
+              {row.value} (avg)
+            </span>
+          );
+        }
+      },
+      {
+        Header: "Visits",
+        accessor: "visits",
+        aggregate: vals => _.sum(vals)
+      }
+    ]
+  }
+]
+
 class Grid extends React.Component {
   constructor() {
     super();
@@ -11,50 +50,14 @@ class Grid extends React.Component {
       data: makeData()
     };
   }
+  
   render() {
     const { data } = this.state;
     return (
       <div>
         <ReactTable
           data={data}
-          columns={[
-            {
-              Header: "Name",
-              columns: [
-                {
-                  Header: "First Name",
-                  accessor: "firstName"
-                },
-                {
-                  Header: "Last Name",
-                  id: "lastName",
-                  accessor: d => d.lastName
-                }
-              ]
-            },
-            {
-              Header: "Info",
-              columns: [
-                {
-                  Header: "Age",
-                  accessor: "age",
-                  aggregate: vals => _.round(_.mean(vals)),
-                  Aggregated: row => {
-                    return (
-                      <span>
-                        {row.value} (avg)
-                      </span>
-                    );
-                  }
-                },
-                {
-                  Header: "Visits",
-                  accessor: "visits",
-                  aggregate: vals => _.sum(vals)
-                }
-              ]
-            }
-          ]}
+          columns={columns}
           pivotBy={["firstName", "lastName"]}
           defaultPageSize={10}
           className="-striped -highlight"
